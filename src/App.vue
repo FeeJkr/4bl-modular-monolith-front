a3863fe5fd0ea559617b39665c632ecad7d31d6f
Sergii Siryi <sergii.siryi97@gmail.com>
2020-08-03 15:17:16 +0200

    add adminlte plugins, css and js to project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <Aside/>
+
+    <div class="content-wrapper" style="margin-left: 250px;">
+      <router-view></router-view>
+    </div>
+
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Aside from "@/components/Aside";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Footer, Aside},
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
